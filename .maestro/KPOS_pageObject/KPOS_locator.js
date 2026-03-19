020fab7565c4f62579d180f9da8af4ea3cd46f18
HOAIBAO <hoaibao.job@gmail.com>
2026-03-19 19:09:38 +0700

    Initialize repository and add KPOS auto test cases

---
+++ .maestro/KPOS_pageObject/KPOS_locator.js
@@ -0,0 +1,14 @@
+output.login = {
+    usernameField: `txt-login-page-txt-login-username
+Mã nhân viên / Email`,
+    passwordField: `txt-login-page-txt-login-password
+Mật khẩu`,
+    submitButton: "btn-login-page-btn-login-submit"
+};
+
+output.pos = {
+    newInvoiceButton: "btn-new-invoice-btn-pos-create-new-invoice",
+    searchProductField: "txt-pos-page-txt-pos-search-product",
+    lineItemCode: "txt-invoice-line-item-code"
+};
+
